a4aeb30336251bda70f7d1e8c359a502365a8437
James Carlson <jxxcarlson@gmail.com>
2017-05-12 13:51:53 -0400

    Prepare for changing json handlers to deal with nesting

--- src/Data/document.elm
+++ src/Data/document.elm
@@ -7,10 +7,38 @@ import Types exposing(..)
 
 type alias DocumentListRecord = { documents: List Document }
 
+-- Nested JSON: https://gist.github.com/hipertracker/36afd3fa89c1f446cddd0a1fd1d53b6b
+-- {
+--   "documents": [
+--     {
+--       "updated_at": "2017-05-06T21:19:05.085975",
+--       "title": "Alba",
+--       "text": "As cool as the pale wet leaves ... ",
+--       "inserted_at": "2017-05-06T21:19:05.028019",
+--       "identifier": "alba",
+--       "id": 1,
+--       "author_identifier": "ezra_pound",
+--       "author": "Ezra Pound"
+--     },
+--     {
+--       "updated_at": "2017-05-06T21:20:38.836250",
+--       "title": "Metro",
+--       "text": "The apparition of these faces in the crowd ...",
+--       "inserted_at": "2017-05-06T21:20:38.836241",
+--       "identifier": "metro",
+--       "id": 2,
+--       "author_identifier": "ezra_pound",
+--       "author": "Ezra Pound"
+--     }
+--   ]
+-- }
+
+-- data : String
+
 -- documentsRecordDecoder : String -> Result String (List Document)
 -- documentsRecordDecoder author_identifier =
 --   decode DocumentListRecord
---     |> Pipeline.required "document"
+--     |> JPipeline.required "documents"
 --     |> Json.Decode.decodeString documentsDecoder author_identifier
 
 documentsRequestDecoder : String -> Result String (List Document)
